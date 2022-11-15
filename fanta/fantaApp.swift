6500440ef6456023e6c6da1a7dd2dcc1ffd53d3d
nalin9 <nalin@schmoozedating.com>
2022-11-15 20:28:10 +0530

    Initial Commit

---
+++ fanta/fantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  fantaApp.swift
+//  fanta
+//
+//  Created by NalinPorwal on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct fantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
